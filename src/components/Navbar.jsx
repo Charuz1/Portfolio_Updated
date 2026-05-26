27f2ddee00e61f4c7bdaaf2ad2ef1aab6d870341
Charuka Herath <herathcwh@gmail.com>
2026-05-26 18:07:17 +0530

    Update portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
                 <div className="flex items-center justify-between h-16">
                     <div className="flex-shrink-0">
                         <a href="#" className="text-xl font-bold text-white tracking-tight">
-                            Portfolio
+                            Charuka Herath
                         </a>
                     </div>
                     <div className="hidden md:block">
